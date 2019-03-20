589e73025c2282890b61b725be034eec916cd90e
domanttas <domantasgiedraitis@gmail.com>
2019-03-20 16:42:56 +0200

    Serving heroku for all routes

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + '/dist/DragonBallsFront')));
+app.use(express.static('dist'));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/DragonBallsFront'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
